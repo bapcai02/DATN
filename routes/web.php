56c5933694dd6d252c4f7407198daa38d679aa77
bapcai02 <hadv62@wru.com.vn>
2021-03-09 17:26:57 +0700

    layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/home', 'Admin\HomeController@index')->name('admin.home');
+});
