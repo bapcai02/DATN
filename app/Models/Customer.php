3eea18605147b7e7d38ae44c50449006d419e2d0
bapcai02 <hadv62@wru.com.vn>
2021-03-12 14:30:09 +0700

    add fontend shop

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    protected $table = 'customers';
+    protected $fillable = [
+        'user_id', 'name', 'email', 'phone', 'address'
+    ];
 }
